af3402bfbc191e2aaf9435edeb5de25bbec94856
mihirmohnish11 <41952790+mihirmohnish11@users.noreply.github.com>
2026-03-28 12:00:43 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+var CACHE_NAME = 'protein-tracker-v1';
+var URLS_TO_CACHE = [
+  '/protein-tracker/protein_tracker.html',
+  '/protein-tracker/manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).then(function(response) {
+        var clone = response.clone();
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(e.request, clone);
+        });
+        return response;
+      }).catch(function() {
+        return caches.match('/protein-tracker/protein_tracker.html');
+      });
+    })
+  );
+});
